33b275007cb2e694aff72c131a5b049e8872a24d
Juan Feris <feris.juan@gmail.com>
2020-11-26 17:32:53 -0500

    Basic Stack Navigation

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { AppearanceProvider } from 'react-native-appearance'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
-export default function App() {
-	return (
-		<AppearanceProvider>
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-			</View>
-		</AppearanceProvider>
-	)
-}
+import { DetailScreen, PostsScreen, UsersScreen } from './screens/index'
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#ffffff',
-		alignItems: 'center',
-		justifyContent: 'center',
+const AppNavigator = createStackNavigator(
+	{
+		Users: {
+			screen: UsersScreen,
+		},
+		Posts: {
+			screen: PostsScreen,
+		},
+		Detail: {
+			screen: DetailScreen,
+		},
 	},
-})
+	{
+		initialRouteName: 'Users',
+	}
+)
+
+export default createAppContainer(AppNavigator)
---
+++ screens/index.js
@@ -0,0 +1,3 @@
+export { default as DetailScreen } from './Detail'
+export { default as PostsScreen } from './Posts'
+export { default as UsersScreen } from './Users'
